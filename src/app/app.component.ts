37d085090d0df20b78f4236ddd1b27da463d90b4
gridik26 <gridi26@hotmail.com>
2021-03-17 19:51:10 +0100

    avancement blog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mon-blog';
+
+  posts = [
+    {
+      title: 'Mon premier post',  
+      content: 'blablablablablablablablablablablablabla',  
+      loveIts: '',  
+      created_at: new Date()
+    },
+    {
+      title: 'Mon deuxième post',  
+      content: 'bliblibliblibliblibliblibli',  
+      loveIts: '',  
+      created_at: new Date()
+    },
+    {
+      title: 'Encore un post',  
+      content: 'bleblebleblebleblebleblebleblebleble',  
+      loveIts: '',  
+      created_at: new Date()
+    },
+  ];
+
 }
---
+++ src/app/post/post.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+
+  @Input() title: string;  
+  @Input() content: string;  
+  @Input() loveIts: string;  
+  @Input() created_at: Date;
+   
+
+  constructor() { 
+  }
+
+  ngOnInit(): void {
+  }
+
+  onLove() {
+    this.loveIts = 'love';
+  }
+
+  onDontLove() {
+    this.loveIts = 'hate';
+  }
+
+}
